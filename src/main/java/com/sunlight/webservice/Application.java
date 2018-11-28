b2e8b4759592acd42f31e20d9c0edb16b7273ee6
sunlightv2g <sunlightv2g@gmail.com>
2018-11-28 13:54:17 +0900

    Application.java

--- src/main/java/com/sunlight/webservice/Application.java
+++ src/main/java/com/sunlight/webservice/Application.java
@@ -1,11 +1,14 @@
 package com.sunlight.webservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing È°¼ºÈ­
+@EnableJpaAuditing // JPA Auditing 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class Application {
 
 	public static void main(String[] args) {
