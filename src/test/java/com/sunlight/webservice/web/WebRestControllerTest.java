5dcd396593ba2616512fb040859d40f261c452e5
sunlightv2g <sunlightv2g@gmail.com>
2019-01-20 09:59:31 +0700

    commiit

--- src/test/java/com/sunlight/webservice/web/WebRestControllerTest.java
+++ src/test/java/com/sunlight/webservice/web/WebRestControllerTest.java
@@ -1,9 +1,7 @@
 package com.sunlight.webservice.web;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,12 +15,4 @@ public class WebRestControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
-    @Test
-    public void Profile확인 () {
-        //when
-        String profile = this.restTemplate.getForObject("/profile", String.class);
-
-        //then
-        assertThat(profile).isEqualTo("local");
-    }
 }
